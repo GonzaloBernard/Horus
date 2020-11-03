b699368584a46784724a8d647a4584c4e58d238d
gonza <gonza.bernard.sf@gmail.com>
2020-11-03 20:46:35 -0300

    Primer commit

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#/">HORUS</b-navbar-brand>
+      <b-navbar-brand href="#/">Productos</b-navbar-brand>
+      <b-navbar-brand href="#/">Armá tu Build</b-navbar-brand>
+      <b-navbar-brand href="#/about">Contacto</b-navbar-brand>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  computed: {},
+  methods: {},
+  components: {},
+};
+</script>
